f10fd7545d620c9b087d8e4144595b8ae966b54a
pedrodanielpereztorres-maker <pedrodanielpereztorres@gmail.com>
2026-03-06 18:12:18 -0400

    cambio

--- rxconfig.py
+++ rxconfig.py
@@ -1,9 +1,12 @@
 import reflex as rx
 import os
 
+db_url = os.environ.get(
+    "DATABASE_URL", "postgresql://postgres:Vv-32273930@db.uofdsyafzndqzhxvaprf.supabase.co:5432/postgres")
+
 config = rx.Config(
     app_name="pagina_web",
-    db_url=os.environ.get("DATABASE_URL", "postgresql://postgres:V32273930@localhost:5432/mi_pagina"),
+    db_url=db_url,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
